92224a3a17dd044aa8b6384f056536bfcab70447
GuillaumeFERNANDESXP <guillaume.fernandes@hotmail.fr>
2021-04-22 10:47:37 +0200

    Update Token with mintable token inheritance

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,7 +2,10 @@ pragma solidity ^0.5.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
-contract Token is ERC20, ERC20Mintable {
-    // ... see "Tokens" for more info
+contract Token is ERC20, ERC20Detailed, ERC20Mintable {
+    constructor() ERC20Detailed("Gold", "GLD", 18) public {
+        _mint(msg.sender, 1000);
+    }
 }
