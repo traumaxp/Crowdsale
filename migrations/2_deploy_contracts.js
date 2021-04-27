e42885efb2ecdc82253d82f665b2a4fc39b243ab
GuillaumeFERNANDESXP <guillaume.fernandes@hotmail.fr>
2021-04-27 16:08:24 +0200

    add addresss in deploy contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,8 @@
 const MyToken = artifacts.require("MyToken");
 const ExampleTokenCrowdsale = artifacts.require("ExampleTokenCrowdsale");
 
-module.exports = async function(deployer, network, accounts) {
-
- 
+module.exports = async function(deployer) {
+  deployer.deploy(MyToken,"Example Token", "EXM", 18).then(function() {
+    return deployer.deploy(ExampleTokenCrowdsale, 500, '0xC652c2c6B36Ce08a7596069F83C74732FE5614AD', MyToken.address , new web3.utils.BN(web3.utils.toWei('200', 'ether')));
+  });
 }
