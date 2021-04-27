35160e6540043b5aca7782392dd345eaac805aeb
GuillaumeFERNANDESXP <guillaume.fernandes@hotmail.fr>
2021-04-27 16:16:14 +0200

    Update contract token

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -2,9 +2,11 @@ pragma solidity ^0.5.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import 'zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
 
 
-contract MyToken is ERC20, ERC20Detailed {
+
+contract MyToken is ERC20, ERC20Detailed, MintableToken {
   uint public INITIAL_SUPPLY = 200000000;
 
   constructor(string memory _name, string memory _symbol, uint8 _decimals)
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
 const MyToken = artifacts.require("MyToken");
 const ExampleTokenCrowdsale = artifacts.require("ExampleTokenCrowdsale");
 
-module.exports = async function(deployer) {
+module.exports = async function(deployer, accounts) {
   deployer.deploy(MyToken,"Example Token", "EXM", 18).then(function() {
-    return deployer.deploy(ExampleTokenCrowdsale, 500, '0xC652c2c6B36Ce08a7596069F83C74732FE5614AD', MyToken.address , new web3.utils.BN(web3.utils.toWei('200', 'ether')));
+    return deployer.deploy(ExampleTokenCrowdsale, 500, '0x7fC23F3411842f9381eAD4e75Dac9640a1abEB82', MyToken.address , new web3.utils.BN(web3.utils.toWei('200', 'ether')));
   });
 }
