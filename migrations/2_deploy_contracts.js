c4ed6104dbf7eab23c8b4032c961f1ce17ae9492
GuillaumeFERNANDESXP <guillaume.fernandes@hotmail.fr>
2021-04-28 09:54:29 +0200

    Update sol

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,5 +4,6 @@ const ExampleTokenCrowdsale = artifacts.require("ExampleTokenCrowdsale");
 module.exports = async function(deployer, accounts) {
   deployer.deploy(MyToken,"Example Token", "EXM", 18).then(function() {
     return deployer.deploy(ExampleTokenCrowdsale, 500, '0x7fC23F3411842f9381eAD4e75Dac9640a1abEB82', MyToken.address , new web3.utils.BN(web3.utils.toWei('200', 'ether')));
+    MyToken
   });
 }
